aa5be8bd3d2b9cb4d40cb6a841f2fee51d6c5ae9
chLi <565353780@qq.com>
2021-06-28 16:05:05 +0800

    use sed to auto replace code to support opencv4

--- install.sh
+++ install.sh
@@ -1,40 +1,17 @@
-# Voxblox++
-
-## Prepair the environment
-```bash
-sudo apt install python3-dev python3-pip python3-wstool protobuf-compiler dh-autoreconf
+sudo apt install python3-dev python3-pip python3-wstool protobuf-compiler dh-autoreconf -y
 sudo pip3 install -U numpy protobuf scipy scikit-image ipython keras wrapt simplejson netaddr osrf-pycommon scipy pillow catkin_pkg rospkg opencv-python empy
 sudo pip3 install -U tensorflow-gpu
-echo "\"# voxblox++
-export ROS_VERSION=noetic # (Ubuntu 16.04: kinetic, Ubuntu 18.04: melodic, Ubuntu 20.04 noetic)
-export CATKIN_WS=~/catkin_ws\" >> ~/.bashrc"
-```
 
-## Init ROS workspace
-```bash
-mkdir -p $CATKIN_WS/src && cd $CATKIN_WS
+mkdir -p ~/catkin_ws/src && cd ~/catkin_ws
 catkin init
-catkin config --extend /opt/ros/$ROS_VERSION --merge-devel
+catkin config --extend /opt/ros/noetic --merge-devel
 catkin config --cmake-args -DCMAKE_CXX_STANDARD=14 -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=Yes
 wstool init src
-```
 
-## Clone git package
-```bash
 cd src
 git clone ssh://git@chLi:30001/ustc/voxblox-plusplus.git
 wstool merge -t . voxblox-plusplus/voxblox-plusplus_https.rosinstall
 wstool update
-```
 
-## Change the source code to support OpenCV4
-```bash
 sed -i -e 's/CV_BGR2GRAY/cv::COLOR_BGR2GRAY/g' -e 's/CV_CHAIN_APPROX_NONE/cv::CHAIN_APPROX_NONE/g' -e 's/CV_FILLED/cv::FILLED/g' depth_segmentation/depth_segmentation/src/depth_segmentation.cpp
-```
 
-## Build the project
-```bash
-catkin build mask_rcnn_ros depth_segmentation gsm_node
-```
-
-## Enjoy it~
