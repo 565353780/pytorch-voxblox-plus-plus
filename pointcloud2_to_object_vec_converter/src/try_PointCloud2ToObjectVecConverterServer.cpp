e51b91f2a7169cb8891ca1c2fb8b667fde796edf
565353780 <565353780@qq.com>
2022-01-21 13:36:07 +0800

    [RUN] now will use viewpoint_extractor for log info

--- pointcloud2_to_object_vec_converter/src/try_PointCloud2ToObjectVecConverterServer.cpp
+++ pointcloud2_to_object_vec_converter/src/try_PointCloud2ToObjectVecConverterServer.cpp
@@ -14,9 +14,9 @@ int main(int argc, char** argv)
   ros::ServiceClient try_pointcloud_to_objects_client =
     nh.serviceClient<pointcloud2_to_object_vec_converter::PC2ToOBJS>("pointcloud2_to_object_vec_converter/convert_pointcloud2_to_object_vec");
 
-  ROS_INFO("Start call pointcloud_to_objects_server service...");
+  std::cout << "Start call pointcloud_to_objects_server service...\n";
 
-  ROS_INFO("Start wait pointcloud2_to_object_vec_converter_server...");
+  std::cout << "Start wait pointcloud2_to_object_vec_converter_server...\n";
   pointcloud2_to_object_vec_converter::PC2ToOBJS first_get_object_vec_serve;
   while(!try_pointcloud_to_objects_client.call(first_get_object_vec_serve))
   {
@@ -25,26 +25,20 @@ int main(int argc, char** argv)
 
   while(true)
   {
-    ROS_INFO("Start call pointcloud2_to_object_vec_converter_server...");
+    ros::Duration(sleep_duration).sleep();
+
+    std::cout << "Start call pointcloud2_to_object_vec_converter_server...\n";
     pointcloud2_to_object_vec_converter::PC2ToOBJS new_get_object_vec_serve;
 
     if(!try_pointcloud_to_objects_client.call(new_get_object_vec_serve))
     {
-      std::cout << "call pointcloud2_to_object_vec_converter_server failed!" << std::endl;
+      std::cout << "call pointcloud2_to_object_vec_converter_server failed!\n";
 
       return 0;
     }
 
     const std::vector<sensor_msgs::PointCloud2>& objects = new_get_object_vec_serve.response.objects;
     std::cout << "get " << objects.size() << " objects\n";
-    for(const sensor_msgs::PointCloud2& pointcloud2 : objects)
-    {
-      std::cout << "object size = " << pointcloud2.data.size() << std::endl;
-    }
-
-    ROS_INFO("Get response!");
-
-    ros::Duration(sleep_duration).sleep();
   }
 
   return 1;
--- view_point_extractor/src/try_ViewPointExtractorServer.cpp
+++ view_point_extractor/src/try_ViewPointExtractorServer.cpp
@@ -7,43 +7,8 @@
 
 int main(int argc, char** argv)
 {
-  // ObjectSaver object_saver_;
-  //
-  // const float view_point_diff_max = 2;
-  // const size_t view_point_similar_time = 3;
-  // const size_t object_disappear_count_max = 2;
-  //
-  // for(size_t j = 0; j < 9; ++j)
-  // {
-  //   std::cout << "Start Search Valid ViewPoints..." << std::endl;
-  //   object_saver_.resetObjectHistoryMatchState();
-  //
-  //   std::cout << "Start search..." << std::endl;
-  //   for(size_t i = 0; i < 10 - j; ++i)
-  //   {
-  //     BBox3D object_bbox;
-  //     object_bbox.setPosition(i, i, i, i + 1, i + 1, i + 1);
-  //     ViewPoint3D object_view_point;
-  //     object_view_point.setPosition(i, i, i, i + 1, i + 1, i + 1);
-  //
-  //     if(!object_saver_.isObjectViewPointVaild(
-  //           i,
-  //           object_bbox,
-  //           object_view_point,
-  //           view_point_diff_max,
-  //           view_point_similar_time))
-  //     {
-  //       std::cout << "object " << i << "'s viewpoint not valid!" << std::endl;
-  //     }
-  //   }
-  //
-  //   std::cout << "Finish search!" << std::endl;
-  //
-  //   object_saver_.updateObjectHistory(object_disappear_count_max);
-  //   std::cout << "Finish Search Valid ViewPoints..." << std::endl;
-  // }
-  // object_saver_.outputInfo(0);
-  // return 0;
+  // RUN_LOG PARAM
+  const size_t sleep_duration = 20;
 
   ros::init(argc, argv, "try_ViewPointExtractorServer");
 
@@ -51,18 +16,30 @@ int main(int argc, char** argv)
   ros::ServiceClient try_view_point_extractor_client =
       nh.serviceClient<view_point_extractor::PC2ToViewPointVec>("view_point_extractor/get_view_point_vec");
 
-  std::cout << "Start call view_point_extractor_server service..." << std::endl;
+  std::cout << "Start call view_point_extractor_server service...\n";
 
-  view_point_extractor::PC2ToViewPointVec get_view_point_vec_serve;
-  if (!try_view_point_extractor_client.call(get_view_point_vec_serve))
+  std::cout << "Start wait pointcloud2_to_object_vec_converter_server...\n";
+  view_point_extractor::PC2ToViewPointVec first_get_view_point_vec_serve;
+  while(!try_view_point_extractor_client.call(first_get_view_point_vec_serve))
   {
-    std::cout << "call view_point_extractor_server failed!" << std::endl;
-
-    return -1;
+    ros::Duration(sleep_duration).sleep();
   }
 
-  std::cout << "Get view_point_extractor_server response!" << std::endl;
+  while(true)
+  {
+    ros::Duration(sleep_duration).sleep();
 
+    std::cout << "Start call view_point_extractor_server service...\n";
+    view_point_extractor::PC2ToViewPointVec get_view_point_vec_serve;
+    if (!try_view_point_extractor_client.call(get_view_point_vec_serve))
+    {
+      std::cout << "call view_point_extractor_server failed!\n";
+
+      return -1;
+    }
+
+    std::cout << "Get view_point_extractor_server response!\n";
+  }
   return 0;
 }
 
